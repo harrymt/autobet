09e252ef81b4be0198fc94ea69bf866bca10436e
Harry Mumford-Turner <harrymt@me.com>
2015-08-15 21:46:15 +0100

    Setup environment & base code
    Removed unused HTML/JS/SCSS.
    Started javascript writing to HTML, that prints out dummy data.

---
+++ js/dev/player.js
@@ -0,0 +1,31 @@
+
+$(function() {
+    var match = { game: '', odds: '' }; // Define a match object
+
+    // Get matches
+    match.game = 'Swansea v United'; match.odds = '1.2';
+    var matches = [match];
+
+    // Print matches
+    printTable(matches);
+});
+
+
+//
+// Prints matches to output table.
+//
+function printTable (output) {
+    var tableRow = $('<tr></tr>'); // addClass('');
+    for(i = 0; i < output.length; i++){
+        var outputGame = $('<td></td>').text(output[i].game);
+        var outputOdds = $('<td></td>').text(output[i].odds);
+        tableRow.append(outputGame);
+        tableRow.append(outputOdds);
+
+        var outputBetLink = $('<td></td>').append('<a href=#>Bet Now</a>');
+        tableRow.append(outputBetLink);
+    }
+
+    $('#js-output').after(tableRow);
+
+}
