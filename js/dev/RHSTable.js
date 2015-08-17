7f00bd0884140766146148122032e6f34c4066a5
Harry Mumford-Turner <harrymt@me.com>
2015-08-17 13:38:29 +0100

    Split up Results by gameweek, Fixed correct time with results

--- js/dev/RHSTable.js
+++ js/dev/RHSTable.js
@@ -12,49 +12,75 @@ var RHSmatch = {
 // Returns the matches with Odds this gameweek
 //
 function PrintFixtures() {
-    var heading = $('<h1></h1>').text("Game Week 1");
-    var tableHeader = $('<tr>').append(
-        $("<th>").text('Date'),
-        $("<th>").text('Home'),
-        $("<th>").text('Odds'),
-        $("<th>").text('Away'),
-        $("<th>").text('Odds'),
-        $("<th>").text('Result'),
-        $("<th>").text('Correct Score'),
-        $("<th>").text('Correct Result'),
-        $("<th>").text('Over/Under')
-    );
 
-    // 1. Get Fixtures
+
+    // Get Fixtures
     var url = "https://www.kimonolabs.com/api/5ngo4jjk?apikey=RbOBR2dkBMXKj1pxkxWWUYEJperNuBsv";
     $.ajax({
         url: url,
         crossDomain: true,
         dataType: 'jsonp',
         success: function(response) {
-            console.log("Data loaded.");
             console.log(response);
             console.log("Found " + response.results.match.length + " matches");
-
-            var table = $('<table>').append(tableHeader);
-
+            var gameweek = 1;
+            var output = $('<div></div>');
+            var table = $('<table>').append(
+                $('<tr>').append(
+                    $("<th>").text('Date'),
+                    $("<th>").text('Home'),
+                    $("<th>").text('Odds'),
+                    $("<th>").text('Away'),
+                    $("<th>").text('Odds'),
+                    $("<th>").text('Home Result'),
+                    $("<th>").text('Away Result'),
+                    $("<th>").text('Correct Score'),
+                    $("<th>").text('Correct Result'),
+                    $("<th>").text('Over/Under')
+                )
+            );
             for(i = 0; i < response.results.match.length; i++) {
+                if(i % 10 === 0 && i !== 0) {
+                    output.append($('<h1></h1>').text("Game Week " + gameweek++));
+                    output.append(table);
+                    table = $('<table>').append(
+                        $('<tr>').append(
+                            $("<th>").text('Date'),
+                            $("<th>").text('Home'),
+                            $("<th>").text('Odds'),
+                            $("<th>").text('Away'),
+                            $("<th>").text('Odds'),
+                            $("<th>").text('Home Result'),
+                            $("<th>").text('Away Result'),
+                            $("<th>").text('Correct Score'),
+                            $("<th>").text('Correct Result'),
+                            $("<th>").text('Over/Under')
+                        )
+                    );
+                }
+
                 var tableRow = $('<tr></tr>');
                 tableRow.append($('<td>').text(response.results.match[i].date));
                 tableRow.append($('<td>').text(response.results.match[i].hometeam));
                 tableRow.append($('<td>').text('0.0')); //response.results.match[i].homeOdds.toFixed(2)));
                 tableRow.append($('<td>').text(response.results.match[i].awayteam));
                 tableRow.append($('<td>').text('0.0')); //response.results.match[i].awayOdds.toFixed(2)));
-                tableRow.append($('<td>').text(response.results.match[i].result));
+
+                if (response.results.match[i].result != "v") {
+                    tableRow.append($('<td>').text(response.results.match[i].result.split(' - ')[0]));
+                    tableRow.append($('<td>').text(response.results.match[i].result.split(' - ')[1]));
+                } else {
+                    tableRow.append($('<td>').text("-"));
+                    tableRow.append($('<td>').text("-"));
+                }
+
                 tableRow.append($('<td>').text('N')); //response.results.match[i].correctScore));
                 tableRow.append($('<td>').text('N')); //response.results.match[i].correctResult));
                 tableRow.append($('<td>').text('N')); //response.results.match[i].overOrUnder));
                 table.append(tableRow);
             }
 
-            // console.log(table.html());
-            $('#js-output').append(heading);
-            $('#js-output').append(table);
+            $('#js-output').append(output);
         }
     });
 }
